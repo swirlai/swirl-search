2ee26ab116c924dd0d8044e0b349a835ff1a3f3e
Sid <sidprobstein@gmail.com>
2022-10-30 18:27:16 -0400

    Added install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "##S#W#I#R#L##1#.#5##############################################################"
+echo ""
+
+echo "Downloading spacy en_core_web_lg..."
+python -m spacy download en_core_web_lg
+
+echo "Downloading NLTK modules..."
+python -m nltk.downloader stopwords
