f07bf7b98c22c982b8f45a7865923ae467cc36d1
Sid <sidprobstein@gmail.com>
2022-12-04 21:37:04 -0500

    SWIRL SEARCH 1.7
    Fixed Django Admin titles, REST framework title and links

--- swirl/admin.py
+++ swirl/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-
 from .models import SearchProvider, Search, Result
+
+admin.site.site_title = 'SWIRL SEARCH'
+admin.site.site_header = 'Administration Console'
+admin.site.index_title = 'SWIRL SEARCH'
 
 admin.site.register(SearchProvider)
 admin.site.register(Search)
