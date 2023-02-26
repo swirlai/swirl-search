7d8e203665e492e567b0e6c0a5e8df1fc0de6f0f
Sid <sid@swirl.today>
2023-02-26 16:18:44 -0500

    Added csrf context processor?

--- swirl_server/settings.py
+++ swirl_server/settings.py
@@ -78,6 +78,7 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
+                'django.template.context_processors.csrf',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
