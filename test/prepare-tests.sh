6b95a1a4e40696479ddbaf922714d94853dea060
dnicodemus <dave.nicodemus@gmail.com>
2023-03-06 14:32:06 -0500

    straw man for test automation

---
+++ test/prepare-tests.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+PROG=`basename $0`
+
+set -e
+
+echo $PROG Preparing tests
+
+python --version
+
+exit 0
---
+++ test/run-tests.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+PROG=`basename $0`
+
+set -e 
+
+echo $PROG Running tests
+echo $PROG Tests run completed
+
+exit 0
