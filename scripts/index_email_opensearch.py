c39c4eb99535373abe14ac30032963df56a568a9
dnicodemus <dave.nicodemus@gmail.com>
2023-03-15 16:30:35 -0400

    make field size unlimitted to load all of ENRON

--- scripts/index_email_opensearch.py
+++ scripts/index_email_opensearch.py
@@ -29,6 +29,8 @@ def main(argv):
         print(f"Error: file not found: {args.filespec}")
         return
 
+    csv.field_size_limit(sys.maxsize)
+
     f = open(args.filespec, 'r')
     csvr = csv.reader(f, quoting=csv.QUOTE_ALL)
 
@@ -90,8 +92,8 @@ def main(argv):
     # end for
 
 
-#############################################    
-    
+#############################################
+
 if __name__ == "__main__":
     main(sys.argv)
 
