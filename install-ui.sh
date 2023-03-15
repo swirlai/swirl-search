fa2075334f8f189485bb20a2df1f7eec1e17eed1
dnicodemus <dave.nicodemus@gmail.com>
2023-03-15 17:33:22 -0400

    added some doc and remove the existing dir if it's already there.

--- install-ui.sh
+++ install-ui.sh
@@ -1,4 +1,26 @@
 #!/bin/sh
+#
+# Usage:
+#   install-ui.sh [<options>]
+#
+# Install the UI into SWIRL static directory. The SWIRL install and setup must be run
+# before this command can be used.
+#
+# Options:
+#   -h, --help           Display this help message
+# Parse command-line options
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -h|--help) print_help=true ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
+# Print help message if requested
+if [ "$print_help" = true ]; then
+    sed -n '/^# Usage:/,/^$/p' "$0"
+    exit
+fi
 
 PROG=`basename $0`
 
@@ -26,7 +48,7 @@ mkdir $work_dir
 docker create --name $work_container swirlai/spyglass
 docker cp "$work_container:/usr/src/spyglass/ui/dist/spyglass/browser/." $work_dir
 docker rm -f $work_container
-
+rm -rf $target_dir/spyglass
 mkdir $target_dir/spyglass
 cp -r $work_dir/* $target_dir/spyglass
 
