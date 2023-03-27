bf84fe8d10d5a87dd876ed15fb916eadd0cbac53
Sid <sid@swirl.today>
2023-03-27 18:41:30 -0400

    Fix branding

--- swirl/admin.py
+++ swirl/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from .models import SearchProvider, Search, Result
 
-admin.site.site_header = 'SWIRL SEARCH' # title
+admin.site.site_header = 'Swirl Metasearch' # title
 admin.site.index_title = 'Administration Console' # subtitle
 # admin page is index_title | site_title
-admin.site.site_title = 'SWIRL SEARCH' 
+admin.site.site_title = 'Swirl Metasearch' 
 
 admin.site.register(SearchProvider)
 admin.site.register(Search)
