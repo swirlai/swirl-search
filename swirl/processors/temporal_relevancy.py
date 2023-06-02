ca7fe418cabcee4ab307d8cda81eee7978fb5b59
Sid <sid@swirl.today>
2023-06-01 23:31:22 -0400

    DateFinderResultProcessor, TemporalRelevancy working
    DateFinderRP is default. Also added message logging for result processors, still WIP

---
+++ swirl/processors/temporal_relevancy.py
@@ -0,0 +1,70 @@
+'''
+@author:     Sid Probstein
+@contact:    sid@swirl.today
+'''
+
+from django.conf import settings
+
+from swirl.processors.processor import *
+
+#############################################    
+#############################################    
+
+from celery.utils.log import get_task_logger
+logger = get_task_logger(__name__)
+
+import re
+from datetime import datetime, timedelta
+
+class TemporalRelevancyPostResultProcessor(PostResultProcessor):
+
+    type="TemporalRelevancyPostResultProcessor"
+
+    def process(self):
+
+        removed = 0
+        temporally_relevant = 0
+
+        # mark time
+        current_time = datetime.now()
+
+        for result in self.results:
+
+            if result.json_results:
+                temporal_results = []
+
+                for item in result.json_results:
+                    if float(item['swirl_score']) <= settings.SWIRL_MIN_RELEVANCY_SCORE:
+                        removed = removed + 1
+                        continue
+                    # scan for date
+                    if item['date_published'] != 'unknown':
+                        date = datetime.strptime(item['date_published'], '%Y-%m-%d %H:%M:%S')
+                        if settings.SWIRL_MAX_TEMPORAL_DISTANCE_UNITS == 'days':
+                            if current_time - date <= timedelta(days=settings.SWIRL_MAX_TEMPORAL_DISTANCE):
+                                item['explain']['temporal_match'] = item['date_published']
+                                temporal_results.append(item)
+                                temporally_relevant = temporally_relevant + 1
+                            else:
+                                removed = removed + 1
+                            continue
+                        if settings.SWIRL_MAX_TEMPORAL_DISTANCE_UNITS == 'hours':
+                            if current_time - date <= timedelta(hours=settings.SWIRL_MAX_TEMPORAL_DISTANCE):
+                                item['explain']['temporal_match'] = item['date_published']
+                                temporal_results.append(item)
+                                temporally_relevant = temporally_relevant + 1
+                            else:
+                                removed = removed + 1
+                            continue
+                    else:
+                        # logger.warning(f"Failed temporal check: {item['body']}")
+                        removed = removed + 1
+                    # end if
+                # end for
+
+                if result.json_results != temporal_results:
+                    result.json_results = temporal_results
+                    result.save()
+                # end if
+
+        return -1 * removed
---
+++ swirl/processors/write_to_filesystem.py
@@ -0,0 +1,52 @@
+'''
+@author:     Sid Probstein
+@contact:    sid@swirl.today
+'''
+
+from django.conf import settings
+
+from swirl.processors.processor import *
+
+#############################################    
+#############################################    
+
+import json
+import re
+import os
+
+from datetime import datetime
+
+from celery.utils.log import get_task_logger
+logger = get_task_logger(__name__)
+
+class WriteToFileSystemPostResultProcessor(PostResultProcessor):
+
+    type="WriteToFileSystemPostResultProcessor"
+
+    def process(self):
+
+        result_item_list = []
+        for result in self.results:
+            if result.json_results:
+                for item in result.json_results:
+                    result_item_list.append(item)
+
+        base_filename = re.sub(r'\W+', '', self.search.query_string)
+        base_filename = base_filename.replace(' ', '_')
+
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        if settings.SWIRL_WRITE_PATH:
+            target_filename = f"{settings.SWIRL_WRITE_PATH}/{base_filename}_{timestamp}.json"
+        else:
+            self.warning("SWIRL_WRITE_PATH not configured!")
+            target_filename = f"{base_filename}_{timestamp}.json"
+
+        if not os.path.exists(settings.SWIRL_WRITE_PATH):
+            os.makedirs(settings.SWIRL_WRITE_PATH)
+
+        with open(target_filename, 'w') as file:
+            json.dump(result_item_list, file)
+
+        logger.info(f"{self.type}: wrote {len(result_item_list)} records to {target_filename}")
+
+        return len(result_item_list)
