2ae1a8547660cdc47ff5cc795b89c2e0a4e448cb
dnicodemus-la <dave.nicodemus@gmail.com>
2023-07-07 15:40:23 -0400

    remove calls to eval, replace w/ global instantiator

--- swirl/connectors/connector.py
+++ swirl/connectors/connector.py
@@ -30,15 +30,14 @@ from swirl.processors import *
 from swirl.processors.transform_query_processor_utils import get_query_processor_or_transform
 
 SWIRL_PROCESSOR_LIST = SearchProvider.QUERY_PROCESSOR_CHOICES + SearchProvider.RESULT_PROCESSOR_CHOICES + Search.PRE_QUERY_PROCESSOR_CHOICES + Search.POST_RESULT_PROCESSOR_CHOICES
+SWIRL_RP_SKIP_TAG = 'SW_RESULT_PROCESSOR_SKIP'
 
 # Look for this tag in tags and skip any results processor by name using this format
 # SW_RESULT_PROCESSOR_SKIP:<ResultProcessorName>
-SWIRL_RP_SKIP_TAG = 'SW_RESULT_PROCESSOR_SKIP'
-
-SWIRL_OBJECT_DICT = {}
-# DS-612
-for t in SWIRL_PROCESSOR_LIST:
-    SWIRL_OBJECT_DICT[t[0]]=eval(t[0])
+# SWIRL_OBJECT_DICT = {}
+# # DS-612 DONE
+# for t in SWIRL_PROCESSOR_LIST:
+#     SWIRL_OBJECT_DICT[t[0]]=eval(t[0])
 
 ########################################
 ########################################
@@ -183,7 +182,7 @@ class Connector:
         for processor in processor_list:
             logger.info(f"{self}: invoking query processor: {processor}")
             try:
-                processed_query = get_query_processor_or_transform(processor, query_temp, SWIRL_OBJECT_DICT, self.provider.query_mappings, self.provider.tags, self.search_user).process()
+                processed_query = get_query_processor_or_transform(processor, query_temp, SWIRL_PROCESSOR_DISPATCH, self.provider.query_mappings, self.provider.tags, self.search_user).process()
             except (NameError, TypeError, ValueError) as err:
                 self.error(f'{processor}: {err.args}, {err}')
                 return
@@ -330,8 +329,9 @@ class Connector:
             logger.info(f"{self}: invoking processor: process results {processor}")
             last_results = copy.deepcopy(self.results)
             try:
-                # DS-612
-                proc = eval(processor, {"processor": processor, "__builtins__": None}, SWIRL_OBJECT_DICT)(self.results, self.provider, self.query_string_to_provider)
+                # DS-612 DONE
+                # proc = eval(processor, {"processor": processor, "__builtins__": None}, SWIRL_OBJECT_DICT)(self.results, self.provider, self.query_string_to_provider)
+                proc = alloc_processor(processor=processor)(self.results, self.provider, self.query_string_to_provider)
                 modified = proc.process()
                 self.results = proc.get_results()
                 ## Check if this processor generated feed back and if so, remember it.
