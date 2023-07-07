667355844e9a5bea8dfa053c3490b1f6c79b8707
dnicodemus-la <dave.nicodemus@gmail.com>
2023-07-07 15:08:03 -0400

    remove eval from connector alloc, use global instantiation instead

--- swirl/tasks.py
+++ swirl/tasks.py
@@ -23,12 +23,11 @@ module_name = 'tasks.py'
 from swirl.connectors import *
 from swirl.models import SearchProvider
 
-SWIRL_OBJECT_LIST = SearchProvider.CONNECTOR_CHOICES
-
-SWIRL_OBJECT_DICT = {}
-# DS-612
-for t in SWIRL_OBJECT_LIST:
-    SWIRL_OBJECT_DICT[t[0]]=eval(t[0])
+# SWIRL_OBJECT_LIST = SearchProvider.CONNECTOR_CHOICES
+# SWIRL_OBJECT_DICT = {}
+# # DS-612
+# for t in SWIRL_OBJECT_LIST:
+#     SWIRL_OBJECT_DICT[t[0]]=eval(t[0])
 
 ##################################################
 ##################################################
@@ -37,8 +36,9 @@ for t in SWIRL_OBJECT_LIST:
 def federate_task(search_id, provider_id, provider_connector, update, session):
     logger.info(f"{module_name}: federate_task: {search_id}_{provider_id}_{provider_connector} update: {update}")
     try:
-        # DS-612
-        connector = eval(provider_connector, {"provider_connector": provider_connector, "__builtins__": None}, SWIRL_OBJECT_DICT)(provider_id, search_id, update)
+        # DS-612 DONE
+        # connector = eval(provider_connector, {"provider_connector": provider_connector, "__builtins__": None}, SWIRL_OBJECT_DICT)(provider_id, search_id, update)
+        connector = alloc_connector(connector=provider_connector)(provider_id, search_id, update)
         connector.federate(session)
     except NameError as err:
         message = f'Error: NameError: {err}'
