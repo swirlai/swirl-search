f00f1c9ad43c8a5dafda7a2eb4249441d5ded7ce
dnicodemus-la <dave.nicodemus@gmail.com>
2023-07-07 15:33:38 -0400

    remove calls to eval, replace w/ global instantiate

--- swirl/mixers/__init__.py
+++ swirl/mixers/__init__.py
@@ -3,8 +3,15 @@
 @contact:    sid@swirl.today
 '''
 
-from swirl.mixers.relevancy import * 
-from swirl.mixers.date import * 
+from swirl.mixers.relevancy import *
+from swirl.mixers.date import *
 from swirl.mixers.stack import *
+
+def alloc_mixer(mixer):
+    if not mixer:
+        logger.error("blank mixer")
+        return None
+    return globals()[mixer]
+
 
 # Add new mixers here!
