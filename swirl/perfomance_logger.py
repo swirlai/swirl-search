8e51f58c9362250a672c3ffa9ef14d8952b14dc5
dnicodemus-la <dave.nicodemus@gmail.com>
2023-07-19 17:23:34 -0400

    fix constrcutor for perf log to set request ID

--- swirl/perfomance_logger.py
+++ swirl/perfomance_logger.py
@@ -6,7 +6,7 @@ from django.conf import settings
 logger = logging.getLogger(__name__)
 
 class SwirlQueryRequestLogger:
-    def __init__(self, query, providers, start_time=None, request_id=''):
+    def __init__(self, query, providers, start_time=None, request_id=None):
         self.request_id = request_id if request_id is not None else str(uuid.uuid4())
         self.start_time = start_time if start_time is not None else time.time()
         self.providers = providers
