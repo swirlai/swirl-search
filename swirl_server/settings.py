df136592edb220c6380e799f79458659ffe8a42e
Dmitriy Kostenko <dkostenko@orion-alliance.com>
2023-11-04 06:24:24 +0100

    remove unnecessary string

--- swirl_server/settings.py
+++ swirl_server/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import environ
 import os
-from drf_yasg.inspectors import CamelCaseJSONFilter, ReferencingSerializerInspector
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
