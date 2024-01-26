40761a016ebfd2ee2d88edef5c82822e18c46908
Sid <sid@swirl.today>
2024-01-25 23:17:37 -0500

    Add EVENT result_mapping which doubles titles for configured SPs
    We recommend applying to Outlook Calendar

--- swirl/swirl_common.py
+++ swirl/swirl_common.py
@@ -1,4 +1,4 @@
 """
 A place for defs share beteween connectors and processors
 """
-RESULT_MAPPING_COMMANDS = [ 'NO_PAYLOAD', 'FILE_SYSTEM', 'LC_URL' ]
+RESULT_MAPPING_COMMANDS = [ 'NO_PAYLOAD', 'FILE_SYSTEM', 'EVENT', 'LC_URL' ]
