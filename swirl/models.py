f27862721d2c8fd10a9fe961e77804d360c5b5dd
Sid <sid@swirl.today>
2024-01-28 21:28:38 -0500

    Add model CHOICE

--- swirl/models.py
+++ swirl/models.py
@@ -110,6 +110,7 @@ class SearchProvider(models.Model):
         ('CleanTextResultProcessor','CleanTextResultProcessor'),
         ('RequireQueryStringInTitleResultProcessor','RequireQueryStringInTitleResultProcessor'),
         ('DropIrrelevantPostResultProcessor','DropIrrelevantPostResultProcessor'),
+        ('AutomaticPayloadMapperResultProcessor', 'AutomaticPayloadMapperResultProcessor'),
         ('CosineRelevancyResultProcessor','CosineRelevancyResultProcessor')
     ]
     response_mappings = models.CharField(max_length=2048, default=str, blank=True)
