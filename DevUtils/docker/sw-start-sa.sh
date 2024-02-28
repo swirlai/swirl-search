086e91e7ed3f5e0e775937891949cc9f4bc803eb
dnicodemus-la <dave.nicodemus@gmail.com>
2024-02-28 17:24:00 -0500

    all works for me now

--- DevUtils/docker/sw-start-sa.sh
+++ DevUtils/docker/sw-start-sa.sh
@@ -3,14 +3,16 @@
 # Start Redis in the background
 redis-server &
 
-./install.sh
+service nginx start
 
 # Your original command to setup and start the application
 rm -fr ./.swirl && python swirl.py setup && mkdir -p static/api/config &&
 /usr/bin/jq ".default" ./config-swirl-demo.db.json | sed -e "s/<msal-app-id>/$MSAL_APP_ID/" \
     -e "s/<msal-tenant-id>/$MSAL_TENANT_ID/" \
+    -e "s/http:\/\/<msal-host>/https:\/\/<msal-host>/" \
     -e "s/<msal-port>/$MSAL_CB_PORT/" \
-    -e "s/<msal-host>/$MSAL_HOST/" > static/api/config/default &&
+    -e "s/<msal-host>/$MSAL_HOST/" \
+    -e "s/ws:/wss:/" > static/api/config/default &&
 python swirl.py start celery-worker celery-beats &&
 daphne -b 0.0.0.0 -p 8000 swirl_server.asgi:application
 
