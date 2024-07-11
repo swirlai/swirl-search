6badce21b7c540814b8fcf039234967f0b962e2f
Erik Spears <erik@swirl.today>
2024-07-11 16:27:03 -0400

    realign ascii banner

--- swirl/banner.py
+++ swirl/banner.py
@@ -12,7 +12,7 @@ class bcolors:
 
 SWIRL_VERSION = '3.6.0'
 
-SWIRL_BANNER_TEXT = "__S_W_I_R_L__3_._6_._0______________________________________________________"
+SWIRL_BANNER_TEXT = "__S_W_I_R_L__3_._6_._0________________________________________________________"
 SWIRL_BANNER = f'{bcolors.BOLD}{SWIRL_BANNER_TEXT}{bcolors.ENDC}'
 
 #############################################
