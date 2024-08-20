5a29f889439cef46e16d2388fdc418bb895f2743
Erik Spears <erik@swirl.today>
2024-08-20 11:38:25 -0400

    update SWIRL domain throughout

--- swirl/connectors/postgresql.py
+++ swirl/connectors/postgresql.py
@@ -21,7 +21,7 @@ logger = get_task_logger(__name__)
 try:
     import psycopg2
 except ImportError as e:
-    logger.error(f"postgresql.py: Error: can't load psycopg2: {e}, see https://docs.swirl.today/Developer-Reference.html#postgresql")
+    logger.error(f"postgresql.py: Error: can't load psycopg2: {e}, see https://docs.swirlaiconnect.com/Developer-Reference.html#postgresql")
 
 from swirl.connectors.db_connector import DBConnector
 
